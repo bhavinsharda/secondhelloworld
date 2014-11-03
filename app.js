2fdbf0b1606191f4e53c50e3521039842cac1665
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2014-11-03 17:37:17 +0200

    Indentation & comment

--- app.js
+++ app.js
@@ -3,22 +3,24 @@
  * 
  */
 /* jshint node: true */
+
+// Load express
 var express = require('express');
 
 
 // Configure express
 var app = express();
 app.configure(function() {
-	app.use(app.router);
+    app.use(app.router);
 });
 
 
 // Add basic route
 app.get('/hello/:who?', function(req, res) {
-	res.end('Hello '+(req.params.who || 'world')+'.');
+    res.end('Hello '+(req.params.who || 'world')+'.');
 });
 
 
 // Everything's ready. Listen.
 app.listen(8080);
-console.log('Server running.');
+console.log('Server running at port 8080.');
